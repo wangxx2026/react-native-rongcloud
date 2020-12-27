eac0d31d08b82f55c1662d0860054203d423b215
hinjin <hinjin@innben.com>
2020-12-27 15:31:33 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 import { requireNativeComponent } from 'react-native';
+import {
+    NativeModules,
+} from "react-native";
 
 const RongcloudManager = requireNativeComponent('RongcloudManager', null);
 
 export default RongcloudManager;
+
+export const showHideContainer = () => {
+    return NativeModules.RongcloudModule && NativeModules.RongcloudModule.showHideContainer && NativeModules.RongcloudModule.showHideContainer(false);
+}
+
+export const connectIM = (token) => {
+    return NativeModules.RongcloudModule && NativeModules.RongcloudModule.connectIM && NativeModules.RongcloudModule.connectIM(token);
+}
+
+export const isPreLoginResultValid = () => {
+    return NativeModules.RongcloudModule && NativeModules.RongcloudModule.isPreLoginResultValid && NativeModules.RongcloudModule.isPreLoginResultValid();
+}
+
+export const eAccountLogin = () => {
+    return NativeModules.RongcloudModule && NativeModules.RongcloudModule.eAccountLogin && NativeModules.RongcloudModule.eAccountLogin();
+}
+
+export const preGetToken = () => {
+    return NativeModules.RongcloudModule && NativeModules.RongcloudModule.preGetToken && NativeModules.RongcloudModule.preGetToken();
+}
